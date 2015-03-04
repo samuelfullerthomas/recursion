93f77eaed899108d1d69c9b53db20108f4b60f70
Samuel Thomas <samuelfullerthomas@gmail.com>
2015-03-03 22:39:27 -0600

    Found the right file

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,5 +6,21 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className){
   // your code here
-  
+  var nodes = [];
+  node = node || document.body;
+
+  var parts = node.className.split(' ');
+
+  if (parts.indexOf(className) >= 0){
+  	nodes.push(node);
+  }
+
+  for(var i = 0; i < node.children.length; i++){
+
+  	var childResults = getElementsByClassName(className, node.children[i]);
+  	nodes = nodes + childResults;
+  	
+  }
+
+  return nodes;
 };
